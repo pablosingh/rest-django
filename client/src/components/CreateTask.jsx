7f10fbee686fd21642b0975e58c01dd3198c36a2
pablosingh <pablo22_4@hotmail.com>
2024-11-12 21:55:45 -0300

    CreateTask

--- client/src/components/CreateTask.jsx
+++ client/src/components/CreateTask.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CreateTask() {
-  return (
-    <div>CreateTask</div>
+    const initialData = {
+        title: "",
+        description: "",
+        done: false
+    }
+    const [ data, setData ] = useState(initialData);
+    const changing = e => {
+        console.log(e);
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        });
+    }
+    const sending = e => {
+        e.preventDefault();
+        console.log(data);
+    }
+    return (
+        <form>
+            <input type="text" name="title" value={data.title}
+                        className="" onChange={changing}/>
+            <input type="text" name="description" value={data.description}
+                        className="" onChange={changing}/>
+            <input type="checkbox" name="done" checked={data.done}
+                        className="" onChange={changing}/>
+            <button onClick={sending}>Enviar</button>
+        </form>
   )
 }
